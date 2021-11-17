d6befbce2fde42d7c3b51d9211359324557f07ec
Erin Quinn <equinn125@gmail.com>
2021-11-16 20:47:29 -0700

    refactor controller

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,17 +1,15 @@
 class Api::V1::RoadTripController<ApplicationController
-
+  before_action :require_key
   def create
-    user = User.find_by(api_key: params[:api_key])
     if bad_params
       render json: { errors: 'Invalid parameters' }, status: 400
-    elsif user && time
+    elsif  time
       coords = LocationFacade.coords(params[:destination])
       forecast = WeatherFacade.get_destination_weather(coords.latitude, coords.longitude, time)
       render json: RoadTripSerializer.new_trip(params[:destination],params[:origin], time, forecast), status: 200
-    elsif user && time.nil?
+    elsif  time.nil?
       render json: RoadTripSerializer.no_route(params[:destination],params[:origin]), status: 200
       else
-      render json: { errors: 'Unathorized' }, status: 401
     end
   end
 
@@ -22,5 +20,13 @@ class Api::V1::RoadTripController<ApplicationController
 
   def bad_params
     params[:destination].nil? || params[:origin].nil?
+  end
+
+  def key_exists
+    user = User.find_by(api_key: params[:api_key])
+  end
+
+  def require_key
+    render json: { errors: 'Unathorized' }, status: 401 unless key_exists
   end
 end
